a9b8e8d95edf41dfb4f120a0e3c1685ec8fa5bd1
Official Awtlaw <Official Awtlaw@DESKTOP-S8FOKAV>
2023-03-03 13:44:05 -0500

    Add project files.

---
+++ Voter_registration_attendance/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCheckStatus = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.txtResults = New System.Windows.Forms.TextBox()
+        Me.txtRegistered = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'btnCheckStatus
+        '
+        Me.btnCheckStatus.Location = New System.Drawing.Point(267, 361)
+        Me.btnCheckStatus.Name = "btnCheckStatus"
+        Me.btnCheckStatus.Size = New System.Drawing.Size(152, 23)
+        Me.btnCheckStatus.TabIndex = 0
+        Me.btnCheckStatus.Text = "Check Status"
+        Me.btnCheckStatus.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(466, 361)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(210, 97)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Age:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(210, 256)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Age:"
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(267, 94)
+        Me.txtAge.Multiline = True
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(274, 51)
+        Me.txtAge.TabIndex = 4
+        '
+        'txtResults
+        '
+        Me.txtResults.Location = New System.Drawing.Point(267, 253)
+        Me.txtResults.Multiline = True
+        Me.txtResults.Name = "txtResults"
+        Me.txtResults.ReadOnly = True
+        Me.txtResults.Size = New System.Drawing.Size(274, 85)
+        Me.txtResults.TabIndex = 5
+        '
+        'txtRegistered
+        '
+        Me.txtRegistered.FormattingEnabled = True
+        Me.txtRegistered.Items.AddRange(New Object() {"Not Registered Voter", "Registered "})
+        Me.txtRegistered.Location = New System.Drawing.Point(267, 187)
+        Me.txtRegistered.Name = "txtRegistered"
+        Me.txtRegistered.Size = New System.Drawing.Size(129, 21)
+        Me.txtRegistered.TabIndex = 6
+        Me.txtRegistered.Text = "Not Registered Voter"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtRegistered)
+        Me.Controls.Add(Me.txtResults)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCheckStatus)
+        Me.Name = "Form1"
+        Me.Text = "Voter Registration Attendance"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCheckStatus As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtResults As TextBox
+    Friend WithEvents txtRegistered As ComboBox
+End Class
---
+++ Voter_registration_attendance/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnCheckStatus_Click(sender As Object, e As EventArgs) Handles btnCheckStatus.Click
+        Const VoteMsg As String = "You can vote."
+        Const RegisterMsg As String = "You need to register before you can vote."
+        Const TooYoungMsg As String = "You are too young to Vote."
+        Dim Age As Integer
+        Dim registered As String = String.Empty
+        Integer.TryParse(txtAge.Text, Age)
+        registered = txtRegistered.Text.ToUpper
+        If Age >= 18 Then
+            If registered = "Y" Then
+                txtResults.Text = VoteMsg
+            Else
+                txtResults.Text = RegisterMsg
+            End If
+        Else
+            txtResults.Text = TooYoungMsg
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Display a message box to confirm whether the user wants to exit
+        Dim msgResponse As MsgBoxResult, Cancel
+        msgResponse = MsgBox("Are you sure you want to exit?", vbQuestion + vbYesNo, "Confirm Exit")
+
+        ' If the user clicks No, cancel the form closing event
+        If msgResponse = vbNo Then
+            Cancel = True
+        ElseIf msgResponse = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+End Class
